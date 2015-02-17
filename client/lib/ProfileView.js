326a5fe2ce78f6beaa02c375ffe49757d49d74db
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-02-17 17:14:38 +0000

    profile view update by user

--- client/lib/ProfileView.js
+++
@@ -1,8 +0,0 @@
-var IS_USER_PROFILE = 'isMyProfile';
-Session.setDefault(IS_MY_PROFILE, false);
-
-Template.profileView.helpers({
-  isMyProfile: function(){
-    return Session.get(IS_USER_PROFILE);
-  }
-});
