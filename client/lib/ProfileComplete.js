b7f0b4a16fdc7bb0393833d74b211b578abd11fe
Arnaud Bertrand <arnaud.bertrand.15@eigsi.fr>
2015-02-22 16:18:02 +0000

    profile complete modifs

--- client/lib/ProfileComplete.js
+++ client/lib/ProfileComplete.js
@@ -1,89 +1,22 @@
 Template.profileComplete.events({
-	'click .user-fname button': function(event, template){
-		var $show = template.$('.user-fname .display-none');
-		var $hide = template.$('.user-fname .displayed');
-
-		$show.removeClass('display-none').addClass('displayed');
-		$hide.removeClass('displayed').addClass('display-none');
-
-		if(template.$('.user-fname button span').hasClass('glyphicon-edit')){
-			template.$('.user-fname button span').removeClass('glyphicon-edit').addClass('glyphicon-ok');
-		} else {
-			template.$('.user-fname button span').removeClass('glyphicon-ok').addClass('glyphicon-edit');
-// Here is value to get			
-		}
+	'click .btn-edit':function(event,template){
+		// Hide text and show inputs
+		template.$('.table-user-information .edit').removeClass('display-none');
+		template.$('.table-user-information .field').addClass('display-none');
+		// Hide edit, show validate and cancel
+		template.$('.btn-edit').addClass('display-none');
+		template.$('.btn-cancel').removeClass('display-none');
+		template.$('.btn-validate').removeClass('display-none');
 	},
-	'click .user-lname button': function(event, template){
-		var $show = template.$('.user-lname .display-none');
-		var $hide = template.$('.user-lname .displayed');
-
-		$show.removeClass('display-none').addClass('displayed');
-		$hide.removeClass('displayed').addClass('display-none');
-
-		if(template.$('.user-lname button span').hasClass('glyphicon-edit')){
-			template.$('.user-lname button span').removeClass('glyphicon-edit').addClass('glyphicon-ok');
-		} else {
-			template.$('.user-lname button span').removeClass('glyphicon-ok').addClass('glyphicon-edit');
-// Here is value to get			
-		}
+	'click .btn-cancel':function(event,template){
+		template.$('.table-user-information .field').removeClass('display-none');
+		template.$('.table-user-information .edit').addClass('display-none');
+		// Hide validate and cancel, show edit
+		template.$('.btn-edit').removeClass('display-none');
+		template.$('.btn-cancel').addClass('display-none');
+		template.$('.btn-validate').addClass('display-none');
 	},
-	'click .user-gender button': function(event, template){
-		var $show = template.$('.user-gender .display-none');
-		var $hide = template.$('.user-gender .displayed');
-
-		$show.removeClass('display-none').addClass('displayed');
-		$hide.removeClass('displayed').addClass('display-none');
-
-		if(template.$('.user-gender button span').hasClass('glyphicon-edit')){
-			template.$('.user-gender button span').removeClass('glyphicon-edit').addClass('glyphicon-ok');
-		} else {
-			template.$('.user-gender button span').removeClass('glyphicon-ok').addClass('glyphicon-edit');
-// Here is value to get			
-		}
-	},
-	'click .user-email button': function(event, template){
-		var $show = template.$('.user-email .display-none');
-		var $hide = template.$('.user-email .displayed');
-
-		$show.removeClass('display-none').addClass('displayed');
-		$hide.removeClass('displayed').addClass('display-none');
-
-		if(template.$('.user-email button span').hasClass('glyphicon-edit')){
-			template.$('.user-email button span').removeClass('glyphicon-edit').addClass('glyphicon-ok');
-		} else {
-			template.$('.user-email button span').removeClass('glyphicon-ok').addClass('glyphicon-edit');
-// Here is value to get			
-		}
-	},
-	'click .user-phone button': function(event, template){
-		var $show = template.$('.user-phone .display-none');
-		var $hide = template.$('.user-phone .displayed');
-
-		$show.removeClass('display-none').addClass('displayed');
-		$hide.removeClass('displayed').addClass('display-none');
-
-		if(template.$('.user-phone button span').hasClass('glyphicon-edit')){
-			template.$('.user-phone button span').removeClass('glyphicon-edit').addClass('glyphicon-ok');
-		} else {
-			template.$('.user-phone button span').removeClass('glyphicon-ok').addClass('glyphicon-edit');
-// Here is value to get			
-		}
-	},
-	'click .user-description button': function(event, template){
-		var $show = template.$('.user-description .display-none');
-		var $hide = template.$('.user-description .displayed');
-
-		$show.removeClass('display-none').addClass('displayed');
-		$hide.removeClass('displayed').addClass('display-none');
-
-		if(template.$('.user-description button span').hasClass('glyphicon-edit')){
-			template.$('.user-description button span').removeClass('glyphicon-edit').addClass('glyphicon-ok');
-		} else {
-			template.$('.user-description button span').removeClass('glyphicon-ok').addClass('glyphicon-edit');
-// Here is value to get			
-		}
-	},
-	'click .panel-heading .back': function(){
+	'click .btn-back': function(){
 		Router.go("profileView", {name: this.profileUsername});
 	}
 });
