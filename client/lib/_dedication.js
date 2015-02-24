9ff5971b17c809a4a71d500e8b4d512dfb76dc11
antoinesab <antoine.sabri@gmail.com>
2015-02-24 21:25:57 +0100

    reduction de la dedication

--- client/lib/_dedication.js
+++ client/lib/_dedication.js
@@ -15,6 +15,11 @@ Template.dedication.events({
 		template.$('.new-dedication').fadeOut();
 		return false;
 	},
+	'click .dedication-user-image-i': function(event,template){
+		$('.dedication-user-image-i').addClass('img-change-1');
+		setTimeout(function(){ $('.dedication-user-image-i').attr('src','http://img.kazeo.com/132/1329278/XL/chaton-blanc-et-beige.jpg');$('.dedication-user-image-i').removeClass('img-change-1');$('.dedication-user-image-i').addClass('img-change-2');}, 500);
+		return false;
+	},
 	'submit': function(event,template){
 		alert('Message sent not implemented');
 		return false;
